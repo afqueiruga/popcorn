a6d4ee07b3068eff1ee04ef65ef72a086c175fa0
Alejandro Queiruga <afqueiruga@lbl.gov>
2017-06-20 15:33:43 -0700

    Need to force the husks to select 2.7

--- popcorn/boilerplates/pylink.py
+++ popcorn/boilerplates/pylink.py
@@ -124,7 +124,7 @@ find_package(GSL REQUIRED)
 find_package(SWIG REQUIRED)
 include(${{SWIG_USE_FILE}})
 set(CMAKE_SWIG_FLAGS "")
-find_package(PythonLibs)
+find_package(PythonLibs 2.7 REQUIRED)
 include_directories(${{PYTHON_INCLUDE_PATH}})
 include_directories(${{KERNEL_INCLUDES}})
 
