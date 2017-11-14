f9196114f20600b0937fab731118a966419d3574
Alejandro Queiruga <afqueiruga@lbl.gov>
2017-11-14 12:12:15 -0800

    Fields keep track of themselves

--- popcorn/functional.py
+++ popcorn/functional.py
@@ -4,30 +4,18 @@ Generic tools for functional implementation
 """
 from PopcornVariable import PopcornVariable
 from sympy import Matrix, eye
+import popcorn_globals
 
 class Field( PopcornVariable):
     def __new__(cls, name, gdim, rank):
-        #if rank==0:
-        #    C = super(Field,cls).__new__(cls, 'field_'+name, 1,1)
-        #else:
         C = super(Field,cls).__new__(cls, 'field_'+name, gdim,rank)
-        #C.gdim = gdim
         C.pv_grad_u = None
+        popcorn_globals.registered_fields.add(C)
         return C
-    #def __init__(self, name, gdim, rank):
-    #    self.pv_u = PopcornVariable('field_'+name, gdim,rank)
-    #    self.pv_grad_u = None
-    #def val(self):
-    #    return self.pv_u.as_matrix()
     def grad(self):
         if self.pv_grad_u is None:
             self.pv_grad_u = PopcornVariable('grad_'+ self.name, self.dim, self.rank+1)
-        return self.pv_grad_u #.as_matrix()
-    #def emit(self):
-    #    return "\n".join([
-    #        super(Field,self).emit(),
-    #        ( self.pv_grad_u.emit() if self.pv_grad_u is not None else "" )
-    #        ])
+        return self.pv_grad_u
     
 def extract(P, u, grad_u,   NJ, grad_NJ,
                tu, grad_tu, NI, grad_NI):
@@ -36,21 +24,13 @@ def extract(P, u, grad_u,   NJ, grad_NJ,
      + P.diff(tu) * NI
     KIJ = Matrix([RI]).jacobian(grad_u).dot( grad_NJ ) \
       + P.diff(u) * NJ
-    # RI = RI.subs(u, P0.dot(pv_a.as_matrix()) )
-    # for i in range(gdim):
-        # RI = RI.subs(grad_u[i], grad_NI[i] )
-    # KIJ = KIJ.subs(u, P0.dot(pv_a.as_matrix()) )
-    # for i in range(gdim):
-        # KIJ = KIJ.subs(grad_u[i], grad_NJ[i] )
 
     return RI, KIJ
 
-def gateaux(f, u, N, dNdx): # NOTE
-    # from IPython import embed ; embed()
+def gateaux(f, u, N, dNdx):
     R = [ Matrix([f]).diff(ui) * NA + Matrix([f]).jacobian(grad(u)[i,:]) * dNdx[:,A]
               for A,NA in enumerate(N)
               for i,ui in enumerate(u) ]
-    # print len(R)
     # TODO: Reshape this guy
     return Matrix(R)
 
--- popcorn/popcorn_globals.py
+++ popcorn/popcorn_globals.py
@@ -1,3 +1,4 @@
 
 registered_inputs = set()
 registered_outputs = set()
+registered_fields = set()
