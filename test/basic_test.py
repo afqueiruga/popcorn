6868b19bd00e56dec82e8735385318ef764aad38
Alejandro Francisco Queiruga <afqueiruga@lbl.gov>
2018-11-08 10:53:44 -0800

    Typo in that test

--- test/basic_test.py
+++ test/basic_test.py
@@ -12,7 +12,7 @@ class MakeSpringHusk(ut.TestCase):
         x0,x1 = i_x.Vertex_Split()
         y0,y1 = i_y.Vertex_Split()
         norm = lambda a : sqrt( (a.T*a)[0,0] )
-        f = i_k[0] * (norm(y0-y1)-norm(x0-x1))/(norm(y0-y1) * (y1-y0))
+        f = i_k[0] * (norm(y0-y1)-norm(x0-x1))/norm(y0-y1) * (y1-y0)
         R = Matrix([f,-f])
         K = R.jacobian(i_y) # <---- Take a symbolic derivative!
         o_R = Output('R',[Vector],1)
