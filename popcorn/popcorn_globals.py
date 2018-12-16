20d2ade617620185052904b1058f7fd93417d7c3
Alejandro Francisco Queiruga <afqueiruga@lbl.gov>
2018-12-16 13:59:18 -0800

    Context managing

--- popcorn/popcorn_globals.py
+++ popcorn/popcorn_globals.py
@@ -1,5 +1,19 @@
-
+# Popcorn uses a global context to keep track of 
+# inputs and outputs etc. to generate kernels
+# with no bookkeeping by the developer
 registered_inputs = {} #set()
 registered_outputs = {} #set()
 registered_fields = {} #set()
 registered_kernels = set()
+
+def reset_context():
+    """
+    If you're generating lots of husks in one file, you'll
+    probably want to do this when starting a new one.
+    """
+    global registered_inputs,registered_outputs
+    global registered_fields,registered_kernels
+    registered_inputs = {} #set()
+    registered_outputs = {} #set()
+    registered_fields = {} #set()
+    registered_kernels = set()
